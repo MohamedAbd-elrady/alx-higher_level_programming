7da2c6facaa43b3e5de59cfe564b3c0a2019c5bd
MohamedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-11-02 14:52:35 +0200

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
+from sys import argv
+arg_str_0 = "arguments."
+arg_str_1 = "argument:"
+arg_str_n = "arguments:"
+argc = len(argv) - 1
+
+
+def print_args():
+    for i in range(1, len(argv)):
+        print("{:d}: {}".format(i, argv[i]))
+
+
 if __name__ == "__main__":
-    from sys import argv
-    res = ""
-    argc = len(argv) - 1
-    delim = 's:' if argc > 1 else '.' if argc == 0 else ':'
-    for count, arg in enumerate(argv[1:]):
-        res += '{:d}: {}\n'.format(count+1, arg)
-    print('{:d} argument{}\n{}'.format(argc, delim, res), end="")
+    if (argc == 0):
+        print("{:d} {}".format(argc, arg_str_0))
+    elif (argc == 1):
+        print("{:d} {}".format(argc, arg_str_1))
+        print_args()
+    else:
+        print("{:d} {}".format(argc, arg_str_n))
+        print_args()
