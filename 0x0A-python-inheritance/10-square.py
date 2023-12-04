ced1e80163a4b0bb63a4a895bb738cbd91fc2e22
MohamedAbd-elrady <mmohammedabdelrady@gmail.com>
2023-12-04 21:35:47 +0200

    tasks

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
-def lookup(obj):
-    return (0)
-print(lookup(obj))
+
+"""Defines a Rectangle subclass Square."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
