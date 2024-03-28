85a9e0a3a0ccd8e0521ec25278664fdbade55c06
MohamedAbd-elrady <mmohammedabdelrady@gmail.com>
2024-03-28 10:17:34 +0200

    tasks

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,6 +4,9 @@ Function that finds a peak in a list of unsorted integers
 """
 
 
+from re import X
+
+
 def find_peak(list_of_integers):
     """
     finds num that's greater than both left and right
@@ -11,21 +14,21 @@ def find_peak(list_of_integers):
     if len(list_of_integers) == 0:
         return None
 
-    l = list_of_integers
+    x = list_of_integers
     beg = 0
-    end = len(l)-1
+    end = len(x)-1
 
-    if l[beg] > l[beg+1]:
-        return l[beg]
-    if l[end] > l[end-1]:
-        return l[end]
+    if x[beg] > x[beg+1]:
+        return x[beg]
+    if x[end] > x[end-1]:
+        return x[end]
 
     mid = (beg+end)//2
-    if l[mid-1] < l[mid] and l[mid+1] < l[mid]:
-        return l[mid]
-    if l[mid] < l[mid-1]:
-        return find_peak(l[beg:mid+1])
-    elif l[mid] < l[mid+1]:
-        return find_peak(l[mid:end+1])
+    if x[mid-1] < x[mid] and x[mid+1] < x[mid]:
+        return x[mid]
+    if x[mid] < x[mid-1]:
+        return find_peak(x[beg:mid+1])
+    elif x[mid] < x[mid+1]:
+        return find_peak(x[mid:end+1])
     else:
-        return l[beg]
+        return x[beg]
